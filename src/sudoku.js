9917541d56cdd00811125db8ef0562670e6e63e3
Tristen Everett <spock214@gmail.com>
2020-06-15 11:55:59 -0700

    create a checkRow prototype that passes the second test

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,3 +1,7 @@
 export function Sudoku(row1, row2, row3, row4, row5, row6, row7, row8, row9) {
   this.puzzle = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
 }
+
+Sudoku.prototype.checkRow = function(){
+  return false;
+}
