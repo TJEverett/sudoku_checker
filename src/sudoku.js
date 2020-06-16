b745825a69df62dcac018c59b9998834f3b04c8f
Tristen Everett <spock214@gmail.com>
2020-06-16 10:10:40 -0700

    add comment to object constructor, and add paramater to checkRow function to take in which row number to check

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,7 +1,8 @@
 export function Sudoku(row1, row2, row3, row4, row5, row6, row7, row8, row9) {
+  // row1 through row9 are each an array of 9 entries (a row from the sudoku grid left to right)
   this.puzzle = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
 }
 
-Sudoku.prototype.checkRow = function(){
+Sudoku.prototype.checkRow = function(rowNumber){
   return false;
 }
