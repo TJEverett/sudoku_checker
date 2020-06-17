42c76cc5b534a00fba91bb13575819745e1166f8
Tristen Everett <spock214@gmail.com>
2020-06-17 10:00:03 -0700

    update checkRow prototype to pass both second and third tests

--- src/sudoku.js
+++ src/sudoku.js
@@ -4,5 +4,16 @@ export function Sudoku(row1, row2, row3, row4, row5, row6, row7, row8, row9) {
 }
 
 Sudoku.prototype.checkRow = function(rowNumber){
-  return false;
+  let numberCheck = [false, false, false, false, false, false, false, false, false];
+  let placeHolder = 10;
+  for(let a = 0; a <= 8 && a < this.puzzle[rowNumber].length; a++){
+    placeHolder = this.puzzle[rowNumber][a] - 1;
+    numberCheck[placeHolder] = true;
+  }
+  for(let b = 0; b <= 8; b++){
+    if(numberCheck[b] === false){
+      return false;
+    }
+  }
+  return true;
 }
